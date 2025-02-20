29b4945ec7f4d4b7160faabbf271cbd609539dfc
RajK-NeetiAI <raj@neeti.ai>
2025-02-20 17:25:14 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
